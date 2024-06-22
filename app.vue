218873d358fe7a541a891b6a7eb219141f5cc4e9
Quadir <aquadirmuhammad@gmail.com>
2024-06-23 00:27:02 +0100

    text updated

--- app.vue
+++ app.vue
@@ -17,29 +17,29 @@
       </nav>
     </header>
     <section>
-      <UiTypographyH1>Beerus super token</UiTypographyH1>
-      <UiTypographyP>Beerus is the favorite cat of the CEO and author of Dragon Ball. He has super powers, and the CEO of Solana loves cats and their super powers. Tweet about Super Token</UiTypographyP>
+      <UiTypographyH1>Max The Greatest Cat</UiTypographyH1>
+      <UiTypographyP>Max the Greatest Cat is not just another meme token on Solana Blockchain, it's a celebration of feline greatness and meme culture</UiTypographyP>
       <img src="/01.jpeg" class="w-full my-10" alt="Beerus holding beerus token">
       <ul class="grid md:grid-cols-2 mt-10 gap-8">
         <li class="bg-amber-900 p-5 rounded-xl">
           <div class="background w-full h-48 rounded-md bg-oramge-500"></div>
-          <UiTypographyH2 class="mt-4 text-center">Total supply 100million</UiTypographyH2>
+          <UiTypographyH2 class="mt-4 text-center">🔒 LP is burned</UiTypographyH2>
         </li>
         <li class="bg-amber-900 p-5 rounded-xl">
           <div class="background w-full h-48 rounded-md bg-oramge-500"></div>
-          <UiTypographyH2 class="mt-4 text-center">LP burned</UiTypographyH2>
+          <UiTypographyH2 class="mt-4 text-center">⚙️ Mint Authority is revoked.          </UiTypographyH2>
         </li>
         <li class="bg-amber-900 p-5 rounded-xl">
           <div class="background w-full h-48 rounded-md bg-oramge-500"></div>
-          <UiTypographyH2 class="mt-4 text-center">Contract revoked</UiTypographyH2>
+          <UiTypographyH2 class="mt-4 text-center">🥶 Freeze Authority is revoked.          </UiTypographyH2>
         </li>
         <li class="bg-amber-900 p-5 rounded-xl">
           <div class="background w-full h-48 rounded-md bg-oramge-500"></div>
-          <UiTypographyH2 class="mt-4 text-center">Sol trending</UiTypographyH2>
+          <UiTypographyH2 class="mt-4 text-center">💰 Zero buy or sell taxes.</UiTypographyH2>
         </li>
         <li class="bg-amber-900 p-5 rounded-xl">
           <div class="background w-full h-48 rounded-md bg-oramge-500"></div>
-          <UiTypographyH2 class="mt-4 text-center">Massive marketing</UiTypographyH2>
+          <UiTypographyH2 class="mt-4 text-center">🔍 Fully community driven token</UiTypographyH2>
         </li>
         <li class="grid items-center">
           <NuxtLink class="bg-amber-900 p-5 rounded-xl flex items-center gap-5 w-fit mx-auto" to="https://x.com/MaxTheGr8estCat?t=zcQAoQMH6LKlUqWfy20HiQ&s=09" target="_blank">
@@ -54,40 +54,8 @@
       </ul>
     </section>
     <img src="/02.jpeg" class="w-full my-10" alt="">
-    <section>
-      <UiTypographyH1 class="saiyan text-center">Roadmap</UiTypographyH1>
-      <ul class="grid gap-10 lg:grid-cols-3 mt-10">
-        <li class="border border-white p-5">
-          <h2 class="saiyan text-6xl">Phase One</h2>
-          <UiTypographyP class="mt-5">
-            V1 website<br  />
-            Launch on  Raydium<br  />
-            Sol trending<br  />
-            Massive marketing
-          </UiTypographyP>
-        </li>
-        <li class="border border-white p-5">
-          <h2 class="saiyan text-6xl">Phase Two</h2>
-          <UiTypographyP class="mt-5">
-            Descreener update<br  />
-            CMC/CG FasTrack<br  />
-            Partnership<br  />
-            More marketing
-          </UiTypographyP>
-        </li>
-        <li class="border border-white p-5">
-          <h2 class="saiyan text-6xl">Phase Three</h2>
-          <UiTypographyP class="mt-5">
-            BeerussuperSwap<br />
-            Beerus NFTs market place<br />
-            Dexview update<br />
-            Dextool update<br />
-            Tweeter space
-          </UiTypographyP>
-        </li>
-      </ul>
-    </section>
     <img src="/03.jpeg" class="w-full my-10" alt="">
+    <UiTypogrpahyP><span class="supernova">Disclainer</span>an Max the Greatest Cat is a meme token on the Solana chain crested for fun it is not a financial advise take full responsibility of any loss you encounter</UiTypogrpahyP>
     <footer class="grid place-items-center">
       <div class="flex gap-5">
         <NuxtLink to="https://t.me/MaxTheGreatestCat" class="w-12 aspect-square bg-amber-900 hover:bg-purple-500 justify-center duration-300 ease-in-out rounded-full outline-none focus:outline-none flex items-center gap-4">
--- components/Ui/Typography/H1.vue
+++ components/Ui/Typography/H1.vue
@@ -1,3 +1,3 @@
 <template>
-  <h1 class="text-6xl xl:text-8xl saiyan"><slot/></h1>
+  <h1 class="text-6xl xl:text-8xl supernova"><slot/></h1>
 </template>
